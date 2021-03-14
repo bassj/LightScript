20a585e7e260ba1da1c889152720d77da558d323
Jack <jbass2000@gmail.com>
2021-03-14 17:48:40 -0400

    WIP: Emitter updates

--- src/emitter.rs
+++ src/emitter.rs
@@ -37,6 +37,22 @@ impl Encoded for EncodedSection {
     }
 }
 
+struct FuncSection {
+    signature_ids: Vec<u32>
+}
+
+impl Encoded for FuncSection {
+    fn encode(&self) -> Vec<u8> {
+        let mut encoded: Vec<u8> = Vec::new();
+
+        for id in signature_ids {
+            encoded.extend_from_slice(&id.to_wasm_bytes());
+        }
+
+        encoded
+    }
+}
+
 struct EncodedFuncBody {
     locals: Vec<EncodedLocal>,
     instructions: Vec<u8>,
@@ -64,7 +80,19 @@ impl Encoded for EncodedLocal {
 }
 
 struct EncodedModule {
-    code_section: Option<EncodedSection>
+    custom_section: Option<EncodedSection>,
+    type_section: Option<EncodedSection>,
+    import_section: Option<EncodedSection>,
+    function_section: Option<EncodedSection>,
+    table_section: Option<EncodedSection>,
+    memory_section: Option<EncodedSection>,
+    global_section: Option<EncodedSection>,
+    export_section: Option<EncodedSection>,
+    start_section: Option<EncodedSection>,
+    element_section: Option<EncodedSection>,
+    code_section: Option<EncodedSection>,
+    data_section: Option<EncodedSection>,
+    data_count_section: Option<EncodedSection>
 }
 
 impl Encoded for EncodedModule {
@@ -76,7 +104,19 @@ impl Encoded for EncodedModule {
 impl Default for EncodedModule {
     fn default() -> Self {
         Self {
-            code_section: None
+            custom_section: None,
+            type_section: None,
+            import_section: None,
+            function_section: None,
+            table_section: None,
+            memory_section: None,
+            global_section: None,
+            export_section: None,
+            start_section: None,
+            element_section: None,
+            code_section: None,
+            data_section: None,
+            data_count_section: None
         }
     }
 }
@@ -105,18 +145,22 @@ impl ModuleEmitter {
 
     pub fn emit(&self) -> Vec<u8> {
         let mut module = EncodedModule::default();
+        // For right now we're just gonna build a simple module that exports a add function that accepts 2 f32's and returns 1 f32
+        
+        // Lets build the func section.
+        let func_section = FuncSection {
 
-        //let mut prog = webassembly::MAGIC_NUMBER.to_vec();
-        //prog.extend_from_slice(webassembly::VERSION_1); // Module header
+        }
 
-
+        // Lets build the code section. This is the actual implmentation of our add function.
         // We take 2 f32's and return 1, so our locals can just be 3 f32
         let locals = self.encode_local(webassembly::F32, 3);
         let instructions: Vec<u8> = Vec::new();
         let add_func = self.encode_function(vec![locals], instructions);
-        let functions = self.encode_vector(add_func.encode());
 
-        let code_section = self.encode_section(webassembly::SECTION_CODE, functions);
+
+        let func_vec = self.encode_vector(add_func.encode());
+        let code_section = self.encode_section(webassembly::SECTION_CODE, func_vec);
         module.code_section = Some(code_section);
 
         module.encode()
